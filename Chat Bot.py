20efe40bf75d5352741a65ef9873eed30aab9fa2
TKelly05 <90604803+TKelly05@users.noreply.github.com>
2021-09-20 09:36:10 +0100

    Add files via upload

---
+++ Chat Bot.py
@@ -0,0 +1,25 @@
+print("Hello!")
+a = input("What's your name?")
+print("Nice to meet you," + a)
+
+print("So,")
+b = input("How are you doing today?")
+print("That's good to hear!")
+
+print("I have been having a good day so far too!")
+c = input("What are you plans?")
+print("That sounds exciting!")
+
+print("Actually,")
+d = input("What will you be doing there?")
+print("I might have to try that myself.")
+
+e = input("Would you like to know my name? Yes/No?")
+
+if e == "Yes":
+    print("My name is Tobias Jakey the Third")
+
+elif e == "No":
+    print("Well too bad, my name is Tobias Jakey the Third")
+else:
+    print("Sorry. that is not an option!")
