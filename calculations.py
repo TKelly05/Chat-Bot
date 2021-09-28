371f92d49be2779ce619c213a39e657f0a3ee09a
TKelly05 <90604803+TKelly05@users.noreply.github.com>
2021-09-28 13:14:35 +0100

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,25 @@
+def addition(num1, num2):
+    print(num1 + num2)
+
+
+addition(70, 60)
+
+def subtract(num1, num2):
+    print(num1-num2)
+
+
+subtract(70, 60)
+
+def divide(num1, num2):
+    print(num1/num2)
+
+
+
+divide(70, 60)
+
+def multiply(num1, num2):
+    print(num1*num2)
+
+
+
+multiply(70, 60)
