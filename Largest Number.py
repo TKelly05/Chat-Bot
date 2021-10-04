8fa1585eac1347466f06800399d0b66339a7b760
TKelly05 <90604803+TKelly05@users.noreply.github.com>
2021-10-04 10:04:33 +0100

    Add files via upload

---
+++ Largest Number.py
@@ -0,0 +1,13 @@
+
+num1=(70)
+num2=(89)
+num3=(56)
+
+if (num1 > num2) and (num2 < num3):
+    print(num1)
+
+elif (num1 < num2) and (num2 > num3):
+    print(num2)
+
+elif (num1 < num2) and (num2 < num3):
+    print(num3)
