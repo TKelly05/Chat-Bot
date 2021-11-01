0661e560babcc58fec7a539611b813b712d86543
TKelly05 <90604803+TKelly05@users.noreply.github.com>
2021-11-01 10:29:53 +0000

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,20 @@
+list = [1,  2,   3,  4, 5]
+list2 = [89, 34, 56, 42, 100]
+print ("Original List:")
+print(list)
+list2 = list.reverse()
+print ("Reversed List:")
+print(list)
+list2 = list.reverse()
+
+print("Concatenated list :")
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+results = [6, 8 ,9]
+print("Squared Results :")
+result = [i**2 for i in results]
+print(result)
